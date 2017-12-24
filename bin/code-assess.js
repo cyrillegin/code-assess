d9ec944ea32c4baaecf21512651a1cc28431f3c1
Cyrille Gindreau <cyrillegin@gmail.com>
2017-12-24 07:24:10 -0700

    updating rc file and making sure rc files are used properly

--- bin/code-assess.js
+++ bin/code-assess.js
@@ -48,29 +48,40 @@ function scsslint(options, arg) {
 
 function precheck(arg) {
   console.log('Checking: ', arg);
+  let directory = '';
   const fileExists = fs.existsSync(arg);
   if (! fileExists) {
-    console.log('File/Folder does not exist, quitting');
-    throw 'File/Folder does not exist, quitting';
+    console.log('File/Folder does not exist, using current directory');
+    directory = '.';
+  } else {
+    directory = arg;
   }
+  return directory;
 }
 
 const testList = [
   ['eslint', '.eslintrc.json'],
   ['scsslint', '.sass-lint.yml'],
   ['htmlhint', '.htmlhintrc'],
+  ['sonarwhal', '.sonarwhalrc'],
 ];
 
 function configure() {
   console.log('Checking for configs...');
   // Check for code-assess config.
-  const options = JSON.parse(fs.readFileSync(getRC('.code-assessrc.json')));
+  let options;
+  try {
+    options = JSON.parse(fs.readFileSync(getRC('.code-assessrc.json')));
+  } catch (err) {
+    console.log('Error reading code-assesrc file.');
+    throw err;
+  }
   // Fill in any missing options and default them not to run.
   testList.forEach((test) => {
-    if (! test[0] in options) {
+    if (options[test[0]] !== undefined) {
       // Check if there is an already existing rc file for the test.
       const fileExists = fs.existsSync(test[1]);
-      // If there is, use it as overrides.
+      // If there is, add its rc file.
       if (fileExists) {
         options[test] = {
           run: true,
@@ -83,20 +94,22 @@ function configure() {
         };
       }
     } else {
+      if (options[test[0]] === undefined) {
+        options[test[0]] = {
+          run: false,
+        };
+      }
       // If no overrides have been added, use default.
-      if (! 'rc' in options[test[0]] || options[test[0]].rc.length === 0) {
+      if (options[test[0]].rc === undefined || options[test[0]].rc.length === 0) {
         options[test[0]].rc = getRC(test[1]);
       }
     }
   });
 
-  // TODO: handle overrides
-  console.log('options are: ');
-  console.log(options);
-
   return options;
 }
 
+// This checks to see if there is a local rc file, if not, use ours.
 function getRC(file) {
   return fs.existsSync(file) ? file : `node_modules/code-assess/${file}`;
 }
@@ -105,10 +118,19 @@ function main(arg) {
   precheck(arg);
   options = configure();
 
-  eslint(options, arg);
-  htmlhint(options, arg);
-  scsslint(options, arg);
-  // sonarwhal();
+  console.log(options);
+  if (options.eslint.run) {
+    eslint(options, arg);
+  }
+  if (options.htmlhint.run) {
+    htmlhint(options, arg);
+  }
+  if (options.scsslint.run) {
+    scsslint(options, arg);
+  }
+  if (options.sonarwhal.run) {
+    // sonarwhal();
+  }
 }
 
 const arg = process.argv[2];
